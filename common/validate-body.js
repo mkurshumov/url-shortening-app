8e54cee67bb891e900f415bbc3d570a4bc8c44d1
TAAKUMAZ <martin.kurshumov@swisscom.com>
2022-08-23 00:30:13 +0300

    remove curly braces for better readability

--- common/validate-body.js
+++ common/validate-body.js
@@ -4,28 +4,19 @@ const error = require("./error");
 module.exports = validateBody = (req, res, next) => {
   const { url } = req.body;
 
-  if (!url) {
-    return next(error(400, "Body must have 'url' property."));
-  }
+  if (!url) return next(error(400, "Body must have 'url' property."));
 
-  if (typeof url !== "string" && !Array.isArray(url)) {
+  if (typeof url !== "string" && !Array.isArray(url))
     return next(error(400, "URL type must be array of strings or string."));
-  }
 
   if (Array.isArray(url)) {
-    if (!url.length) {
-      return next(error(400, "URL array is empty."));
-    }
+    if (!url.length) return next(error(400, "URL array is empty."));
 
     const areUrlsValid = url.every((u) => isValidURL(u));
-    if (!areUrlsValid) {
-      return next(error(400, "All URLs must be valid."));
-    }
+    if (!areUrlsValid) return next(error(400, "All URLs must be valid."));
   }
 
-  if (!isValidURL(url)) {
-    return next(error(400, "URL is invalid."));
-  }
+  if (!isValidURL(url)) return next(error(400, "URL is invalid."));
 
   next();
 };
